7c414a3dde2dfda993b2447c8cbbe2e9235dea16
Andre Lima <andre@makemeup.ie>
2022-08-27 12:26:22 +0100

    variaveis.py

---
+++ variaveis.py
@@ -0,0 +1,44 @@
+# a = 1
+# uma_variavel = "um texto"
+
+# variavel_1 = "www"
+# print(variavel_1)
+
+# variavel_print = "Andre"
+# print(variavel_print)
+
+# aluno-python = "Um aluno"
+
+uma_variavel_com_um_nome_grande = 1 # snake_case (pythonico)
+umaVariavelComUmNomeGrande = 1 # camelCase (nao-pythonico)
+
+# abaixo eu imprimo as variaveis
+print(uma_variavel_com_um_nome_grande)
+print(umaVariavelComUmNomeGrande)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
