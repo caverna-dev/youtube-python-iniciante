9205a0f58e884d3eabd05d0ff1cdf266764dccc4
Andre Lima <andre@makemeup.ie>
2022-08-27 12:28:36 +0100

    tipos_de_dados.py

---
+++ tipos_de_dados.py
@@ -0,0 +1,76 @@
+##########################################
+#  Strings (str) - Dados do tipo "texto" #
+##########################################
+nome_usuario = 'Fulano de Tal'
+print(nome_usuario)
+
+tipo_nome_usuario = type(nome_usuario)
+print(tipo_nome_usuario)  # deve imprimir 'str'
+
+
+
+
+
+
+
+
+
+
+
+
+####################################
+# Integer (int) - Números inteiros #
+####################################
+# idade_usuario = 28
+# print(idade_usuario)
+#
+# tipo_idade_usuario = type(idade_usuario)
+# print(tipo_idade_usuario)  # deve imprimir 'int'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#########################################################
+# Float (float) - Números de ponto-flutuante (decimais) #
+#########################################################
+# peso_usuario = 70.0
+# altura_usuario = 1.75
+# print(peso_usuario)
+#
+# tipo_peso_usuario = type(peso_usuario)
+# print(tipo_peso_usuario)  # deve imprimir 'float'
+
+
+
+
+
+
+
+
+
+
+
+######################################################
+# Boolean (bool) - Valor logico: Verdadeiro ou Falso #
+######################################################
+# maior_de_idade = True
+# sobrepeso = False
+# print(maior_de_idade)
+#
+# tipo_maior_de_idade = type(maior_de_idade)
+# print(tipo_maior_de_idade)  # deve imprimir 'bool'
