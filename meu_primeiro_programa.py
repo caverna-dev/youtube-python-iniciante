ad6f8c6a69bd5837a1a41205af5507e57a984ae6
Andre Lima <andre@makemeup.ie>
2022-10-19 20:01:43 +0100

    adicionando comentarios a meu_primeiro_programa.py

--- meu_primeiro_programa.py
+++ meu_primeiro_programa.py
@@ -1,1 +1,7 @@
+# a função 'print()' do python nada mais faz do que imprimir (escrever) o que você
+# passa para ela como parâmetro (entre parênteses) na tela. Só isso.
+# Esta, provavelmente, vai ser a primeira e mais usada função em toda sua carreira no Python! :)
+# O mais comum como primeiro programa é o famoso "Oi, mundo!" ("Hello, World!").
+# Mas aqui no caverna_dev, a gente foge dos clichês :p
+
 print("O Py-ta-ON!")
