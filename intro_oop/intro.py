02b45989679d6dc3e82b9e70938fa2a8ba0aa4a5
André Lima <>
2023-03-11 12:37:08 +0000

    Introducao a Programacao Orientada a Objetos

--- intro_oop/intro.py
+++ intro_oop/intro.py
@@ -1,21 +1,62 @@
-class Contato:
-    def __init__(self, nome, sobrenome, data_nascimento, telefone):
+class Pessoa:
+    MAX_LEN_NOME = 10
+    MAX_LEN_SOBRENOME = 10
+    MAX_LEN_DATA = 10
+
+    def __init__(self, nome, sobrenome, data_nascimento):
         self.nome = nome
         self.sobrenome = sobrenome
         self.data_nascimento = data_nascimento
-        self.telefone = telefone
 
+    def __str__(self):
+        return f"{self.nome_completo}"
+
+    @property
     def nome_completo(self):
         return f"{self.nome} {self.sobrenome}"
 
+    @property
+    def nome(self):
+        return self._nome
+
+    @nome.setter
+    def nome(self, valor):
+        if len(valor) <= self.MAX_LEN_NOME:
+            self._nome = valor
+        else:
+            raise ValueError(f"O atributo 'nome' pode ter no maximo {self.MAX_LEN_NOME}")
+
+
+    @classmethod
+    def metodo_classe(cls):
+        # metodos de classe sao utilziados para manipular a propria class
+        # e nao uma instancia em particular. Um caso de uso comum para
+        # eses tipos de metodos é na criaçao de "factory methods".
+        pass
+
+    @staticmethod
+    def metodo_estatico(arg1, arg2):
+        pass
+
+
+
+class Contato:
+
+    def __init__(self, pessoa, telefone, email):
+        self.pessoa = pessoa
+        self.telefone = telefone
+        self.email = email
+
     def __str__(self):
-        return f"{self.nome_completo()} - {self.telefone}"
-
-contato_1 = Contato("Bruce", "Wayne", "12/12/1980", "11999990000")
-contato_2 = Contato(nome="Bruce", data_nascimento="06/06/1666", telefone="016677889900", sobrenome="Dickinson")
+        return f"{self.pessoa.nome_completo} - {self.telefone} - {self.email}"
 
 
-nome_comp = contato_1.nome_completo()
-# nome_comp = Contato.nome_completo(contato_1)
+p1 = Pessoa("Bruce", "Wayne", "12/12/1980")
+print(p1.nome) # retorna o atributo 'nome'
+p1.nome = "Beiçola da pastelaria" # altera o atributo 'nome'
+print(p1.nome)
 
-print(nome_comp)
+# print(p1)
+# c1 = Contato(p1, "11111111", "bats@gotahm.com")
+# print(c1)
+
